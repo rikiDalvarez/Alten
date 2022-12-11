1b1903012876026dadd498b9737844eb291af110
Ricardo <ric.dalvarez@hotmail.com>
2022-12-11 14:15:37 +0100

    add template product description

--- alten-app/src/pages/ProductList/index.jsx
+++ alten-app/src/pages/ProductList/index.jsx
@@ -22,7 +22,7 @@ function ProductList() {
 				<div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
 					{products.map((product) => (
 						<Link to="/product/:id" className="hover:bg-blue-300 active:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300 rounded-md">
-							<div key={product.id} className="group relative">
+							<div key={product._id} className="group relative">
 								<div className="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-80">
 									<img
 										src="../../../public/iphone.webp"
